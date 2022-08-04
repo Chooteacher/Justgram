decf582fbad417a1325b68234bba0326b8d99fc5
unknown <zmz14s@gmail.com>
2022-08-04 21:35:12 +0900

    js수정

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,28 +1,66 @@
 // 변수 addButton, comment에 각각 button과 input을 저장
-const addButton = document.getElementById("comment-button");
-const comment = document.getElementById("comment");
+// commentList에 comment-list를 저장
+const addButton = Array.from(document.getElementsByClassName("comment-button"));
+const comment = Array.from(document.getElementsByClassName("comment"));
+const commentList = document.getElementsByClassName("comment-list");
 
-// 버튼 클릭,keyup 이벤트리스너
-addButton.addEventListener("click", addcomment);
-comment.addEventListener("keyup", commentKeyUp);
+// click
+addButton.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    const content = comment[index].value;
 
-// 텍스트를 추가하는 함수
-// 텍스트 입력창의 값을 가져와 텍스트 영역에 추가
-function addcomment() {
-  let comment = document.getElementById("comment").value;
-  const commentItem = document.createElement("p");
-  commentItem.classList.add("comment-text");
+    const commentItem = document.createElement("p"); // commentItem라는 변수에 p태그를 만들어 담음
+    commentItem.classList.add("comment-text"); // 만들어놓은 p태그에 comment-text라는 클래스를 추가
 
-  commentItem.innerText = comment;
+    const nameSpan = document.createElement("span");
+    nameSpan.className = "comment-name";
+    nameSpan.textContent = "Choo ";
 
-  document.getElementById("comment-list").appendChild(commentItem);
-  // 텍스트를 추가하고 난 후 input창의 텍스트의 value를 ""(공백)으로 주어 안보이게함
-  document.getElementById("comment").value = "";
-}
-// keyup일때의 함수
+    commentItem.append(nameSpan, content); // 위에서 생성한 p태그에 name과 content를 추가
+
+    commentList[index].appendChild(commentItem); // 해당 index의 commetList에 생성한 p태그 추가
+
+    comment[index].value = ""; // comment를 달고 난 뒤 input창 리셋
+  });
+});
+
+// keyup
 /* enter keycode = 13 */
-function commentKeyUp(e) {
-  if (e.keyCode === 13) {
-    addButton.click();
-  }
-}
+
+comment.forEach((input, index) => {
+  input.addEventListener("keyup", (e) => {
+    if (e.keyCode === 13) {
+      addButton[index].click();
+    }
+  });
+});
+
+//  클릭,keyup 이벤트리스너
+// for (var i = 0; i < addButton.length; i++) {
+//   addButton[i].addEventListener("click", function () {
+//     console.log(i);
+//   });
+// }
+
+//  comment.addEventListener("keyup", commentKeyUp);
+//  텍스트를 추가하는 함수
+//  텍스트 입력창의 값을 가져와 텍스트 영역에 추가
+// function addcomment(e) {
+//    let comment = document.getElementById("comment").value;
+//    -> 이전에 사용한 방법
+
+//    해당 feed에서 comment라는 id값을 가진 요소의 값을 가져옴
+//   -> 변경한 이유 : feed가 여러개일 경우 id값을 가져오는 방식은 맞지않다고 판단함
+//   let comment = e.target.previousElementSibling.value;
+//   const commentItem = document.createElement("p");
+//   commentItem.classList.add("comment-text");
+
+//    commentItem.innerText = comment;
+
+//   document.getElementById("comment-list").appendChild(commentItem);
+//   let a =
+//     e.target.parentNode.previousSibling.previousSibling.previousSibling
+//       .previousSibling.appendChild;
+//   console.log(a);
+
+// }
